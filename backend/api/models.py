1468bb757e2ff75ba7162f7b7bb0ba5c9793f95a
ApaDoctor <apadoctor@gmail.com>
2018-09-26 20:41:09 +0200

    add Currency and ExchangeRate models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=35)
+
+
+class ExchangeRate(models.Model):
+    source = models.ForeignKey(Currency, on_delete=models.PROTECT)
+    target = models.ForeignKey(Currency, on_delete=models.PROTECT)
+    rate = models.FloatField()
+
